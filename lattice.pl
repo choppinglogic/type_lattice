158cdf8dbeb79fce911ac267f513a46fe65aea3a
choppinglogic <daniel0charles0harris@gmail.com>
2023-03-10 10:52:57 -0600

    added strict partial order test

--- lattice.pl
+++ lattice.pl
@@ -1,16 +1,21 @@
-%Sample knowledge base. Use "asserta(related(c,c))" for instance to test "reflexive_relation(related)" - should evaluate to "true."
-:- dynamic related/2.
-related(a,b).
-related(a,a).
-related(b,b).
-related(b,c).
+%Sample knowledge base. "covers(X,Y)" means Y covers X.
+:- dynamic covers/2.
+covers(p,q).
+covers(q,r).
+covers(p,r).
+covers(p,s).
+
+%Utility predicates
 
 %Finds all binary relations with a given functor name in the knowledge base.
 find_all_binary_relations(FunctorName,TermList) :-
     Term =.. [FunctorName|[_,_]],
     findall(Term,Term,TermList).
 
-%Tests to ensure that when X is related to Y and Y does not = X, X is related to X.
+%Order-theoretical property tests
+%Call a property test by specifying a functor name for binary terms in your knowledge base, e.g. "strict_partial_order(covers)"
+
+%Reflexivity test
 reflexive_relation(FunctorName) :-
     find_all_binary_relations(FunctorName,TermList),
     reflexive_terms(TermList,TermList).
@@ -28,7 +33,18 @@ reflexive_terms([Term|Terms],TermList) :-
     member(ReflexiveTerm2,TermList),
     reflexive_terms(Terms,TermList).
 
-%Tests to ensure that X is related to Y and Y is related to X only when X = Y.
+%Irreflexivity test
+irreflexive_relation(FunctorName) :-
+    find_all_binary_relations(FunctorName,TermList),
+    irreflexive_terms(TermList,TermList).
+
+irreflexive_terms([],_).
+irreflexive_terms([Term|Terms],TermList) :-
+    Term =.. [_|[Arg1,Arg2]],
+    \+ (Arg1 = Arg2),
+    irreflexive_terms(Terms,TermList).
+
+%Antisymmetry test
 antisymmetric_relation(FunctorName) :-
     find_all_binary_relations(FunctorName,TermList),
     antisymmetric_terms(TermList,TermList).
@@ -43,3 +59,38 @@ antisymmetric_terms([Term|Terms],TermList) :-
     SymmetricTerm =.. [FunctorName|[Arg2,Arg1]],
     \+ member(SymmetricTerm,TermList),
     antisymmetric_terms(Terms,TermList).
+
+%Transitivity test
+transitive_relation(FunctorName) :-
+    transitive_candidates(FunctorName,TermList,TransitivePairs),
+    all_transitive(TermList,TransitivePairs).
+
+possibly_transitive_pair(TermList,Term1,Term2) :-
+    member(Term1,TermList),
+    member(Term2,TermList),
+    Term1 =.. [FunctorName|[Arg1,Arg2]],
+    Term2 =.. [FunctorName|[Arg2,Arg3]],
+    \+ (Arg1 = Arg2),
+    \+ (Arg2 = Arg3).
+
+transitive_terms(Term1,Term2,Term3) :-
+    Term1 =.. [FunctorName|[Arg1,Arg2]],
+    Term2 =.. [FunctorName|[Arg2,Arg3]],
+    Term3 =.. [FunctorName|[Arg1,Arg3]],
+    \+ (Arg1 = Arg2),
+    \+ (Arg2 = Arg3).
+
+transitive_candidates(FunctorName,TermList,TransitivePairs) :-
+    find_all_binary_relations(FunctorName,TermList),
+    findall([Term1,Term2],possibly_transitive_pair(TermList,Term1,Term2),TransitivePairs).
+
+all_transitive(_,[]).
+all_transitive(TermList,[[Term1,Term2]|TransitivePairs]) :-
+    member(Term3,TermList),
+    transitive_terms(Term1,Term2,Term3),
+    all_transitive(TermList,TransitivePairs).
+
+%Strict partial order test
+strict_partial_order(FunctorName) :-
+    irreflexive_relation(FunctorName),
+    transitive_relation(FunctorName).
