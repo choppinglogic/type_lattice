fec537a73017cada5ab12a94a8bc59f8e111d055
choppinglogic <daniel0charles0harris@gmail.com>
2023-03-20 17:31:49 -0500

    added test for lower bound

--- lattice.pl
+++ lattice.pl
@@ -1,142 +1,180 @@
-%Sample knowledge base. "covers(X,Y)" means Y covers X.
-:- dynamic covers/2.
-covers(p,q).
-covers(q,r).
-covers(p,r).
-covers(p,s).
+%Sample knowledge base.
+:- dynamic divides/2.
+divides(1,1).
+divides(1,2).
+divides(2,2).
+divides(1,3).
+divides(3,3).
+divides(1,4).
+divides(2,4).
+divides(4,4).
+divides(1,5).
+divides(5,5).
+divides(1,6).
+divides(2,6).
+divides(3,6).
+divides(6,6).
 
 %Utility predicates
 
 %Finds all binary relations with a given functor name in the knowledge base.
-find_all_binary_relations(FunctorName,TermList) :-
-    Term =.. [FunctorName|[_,_]],
-    findall(Term,Term,TermList).
+find_all_binary_relations(RelationName,RelationTerms) :-
+    Term =.. [RelationName|[_,_]],
+    findall(Term,Term,RelationTerms).
 
 %Finds all unique arguments in a list of binary relations.
-gather_binary_args(TermList,ArgList) :-
-    gather_binary_args(TermList,[],ArgList).
+gather_binary_args(RelationTerms,RelationArgs) :-
+    gather_binary_args(RelationTerms,[],RelationArgs).
 
-gather_binary_args([],ArgList,ArgList).
-gather_binary_args([Term|Terms],UniqueArgs,ArgList) :-
+gather_binary_args([],RelationArgs,RelationArgs).
+gather_binary_args([Term|Terms],UniqueArgs,RelationArgs) :-
     Term =.. [_|[Arg,Arg]],
     \+ member(Arg,UniqueArgs),
     UniqueArgs1 = [Arg|UniqueArgs],
-    gather_binary_args(Terms,UniqueArgs1,ArgList).
-gather_binary_args([Term|Terms],UniqueArgs,ArgList) :-
+    gather_binary_args(Terms,UniqueArgs1,RelationArgs).
+gather_binary_args([Term|Terms],UniqueArgs,RelationArgs) :-
     Term =.. [_|[Arg,Arg]],
     member(Arg,UniqueArgs),
-    gather_binary_args(Terms,UniqueArgs,ArgList).
-gather_binary_args([Term|Terms],UniqueArgs,ArgList) :-
+    gather_binary_args(Terms,UniqueArgs,RelationArgs).
+gather_binary_args([Term|Terms],UniqueArgs,RelationArgs) :-
     Term =.. [_|[Arg1,Arg2]],
     \+ (Arg1 = Arg2),
     \+ member(Arg1,UniqueArgs),
     \+ member(Arg2,UniqueArgs),
     UniqueArgs1 = [Arg1,Arg2|UniqueArgs],
-    gather_binary_args(Terms,UniqueArgs1,ArgList).
-gather_binary_args([Term|Terms],UniqueArgs,ArgList) :-
+    gather_binary_args(Terms,UniqueArgs1,RelationArgs).
+gather_binary_args([Term|Terms],UniqueArgs,RelationArgs) :-
     Term =.. [_|[Arg1,Arg2]],
     \+ (Arg1 = Arg2),
     \+ member(Arg1,UniqueArgs),
     member(Arg2,UniqueArgs),
     UniqueArgs1 = [Arg1|UniqueArgs],
-    gather_binary_args(Terms,UniqueArgs1,ArgList).
-gather_binary_args([Term|Terms],UniqueArgs,ArgList) :-
+    gather_binary_args(Terms,UniqueArgs1,RelationArgs).
+gather_binary_args([Term|Terms],UniqueArgs,RelationArgs) :-
     Term =.. [_|[Arg1,Arg2]],
     \+ (Arg1 = Arg2),
     member(Arg1,UniqueArgs),
     \+ member(Arg2,UniqueArgs),
     UniqueArgs1 = [Arg2|UniqueArgs],
-    gather_binary_args(Terms,UniqueArgs1,ArgList).
-gather_binary_args([Term|Terms],UniqueArgs,ArgList) :-
+    gather_binary_args(Terms,UniqueArgs1,RelationArgs).
+gather_binary_args([Term|Terms],UniqueArgs,RelationArgs) :-
     Term =.. [_|[Arg1,Arg2]],
     \+ (Arg1 = Arg2),
     member(Arg1,UniqueArgs),
     member(Arg2,UniqueArgs),
-    gather_binary_args(Terms,UniqueArgs,ArgList).
+    gather_binary_args(Terms,UniqueArgs,RelationArgs).
 
-%Order-theoretical property tests
+%Set theoretical tests
+set([]).
+set([Elem|Elems]) :-
+    \+ member(Elem,Elems),
+    set(Elems).
+
+set_inclusion([],_).
+set_inclusion([Term|Terms],RelationTerms) :-
+    member(Term,RelationTerms),
+    set_inclusion(Terms,RelationTerms).
+
+%Order theoretical property tests
 %Call a property test by specifying a functor name for binary terms in your knowledge base, e.g. "strict_partial_order(covers)"
 
 %Reflexivity test
-reflexive_relation(FunctorName) :-
-    find_all_binary_relations(FunctorName,TermList),
-    reflexive_terms(TermList,TermList).
+reflexive_relation(RelationName) :-
+    find_all_binary_relations(RelationName,RelationTerms),
+    reflexive_terms(RelationTerms,RelationTerms).
 
 reflexive_terms([],_).
-reflexive_terms([Term|Terms],TermList) :-
+reflexive_terms([Term|Terms],RelationTerms) :-
     Term =.. [_|[Arg,Arg]],
-    reflexive_terms(Terms,TermList).
-reflexive_terms([Term|Terms],TermList) :-
-    Term =.. [FunctorName|[Arg1,Arg2]],
+    reflexive_terms(Terms,RelationTerms).
+reflexive_terms([Term|Terms],RelationTerms) :-
+    Term =.. [RelationName|[Arg1,Arg2]],
     \+ (Arg1 = Arg2),
-    ReflexiveTerm1 =.. [FunctorName|[Arg1,Arg1]],
-    ReflexiveTerm2 =.. [FunctorName|[Arg2,Arg2]],
-    member(ReflexiveTerm1,TermList),
-    member(ReflexiveTerm2,TermList),
-    reflexive_terms(Terms,TermList).
+    ReflexiveTerm1 =.. [RelationName|[Arg1,Arg1]],
+    ReflexiveTerm2 =.. [RelationName|[Arg2,Arg2]],
+    member(ReflexiveTerm1,RelationTerms),
+    member(ReflexiveTerm2,RelationTerms),
+    reflexive_terms(Terms,RelationTerms).
 
 %Irreflexivity test
-irreflexive_relation(FunctorName) :-
-    find_all_binary_relations(FunctorName,TermList),
-    irreflexive_terms(TermList,TermList).
+irreflexive_relation(RelationName) :-
+    find_all_binary_relations(RelationName,RelationTerms),
+    irreflexive_terms(RelationTerms,RelationTerms).
 
 irreflexive_terms([],_).
-irreflexive_terms([Term|Terms],TermList) :-
+irreflexive_terms([Term|Terms],RelationTerms) :-
     Term =.. [_|[Arg1,Arg2]],
     \+ (Arg1 = Arg2),
-    irreflexive_terms(Terms,TermList).
+    irreflexive_terms(Terms,RelationTerms).
 
 %Antisymmetry test
-antisymmetric_relation(FunctorName) :-
-    find_all_binary_relations(FunctorName,TermList),
-    antisymmetric_terms(TermList,TermList).
+antisymmetric_relation(RelationName) :-
+    find_all_binary_relations(RelationName,RelationTerms),
+    antisymmetric_terms(RelationTerms,RelationTerms).
 
 antisymmetric_terms([],_).
-antisymmetric_terms([Term|Terms],TermList) :-
+antisymmetric_terms([Term|Terms],RelationTerms) :-
     Term =.. [_|[Arg,Arg]],
-    antisymmetric_terms(Terms,TermList).
-antisymmetric_terms([Term|Terms],TermList) :-
-    Term =.. [FunctorName|[Arg1,Arg2]],
+    antisymmetric_terms(Terms,RelationTerms).
+antisymmetric_terms([Term|Terms],RelationTerms) :-
+    Term =.. [RelationName|[Arg1,Arg2]],
     \+ (Arg1 = Arg2),
-    SymmetricTerm =.. [FunctorName|[Arg2,Arg1]],
-    \+ member(SymmetricTerm,TermList),
-    antisymmetric_terms(Terms,TermList).
+    SymmetricTerm =.. [RelationName|[Arg2,Arg1]],
+    \+ member(SymmetricTerm,RelationTerms),
+    antisymmetric_terms(Terms,RelationTerms).
 
 %Transitivity test
-transitive_relation(FunctorName) :-
-    find_all_binary_relations(FunctorName,TermList),
-    transitive_candidates(TermList,PossiblyTransitivePairs),
-    all_transitive(TermList,PossiblyTransitivePairs).
+transitive_relation(RelationName) :-
+    find_all_binary_relations(RelationName,RelationTerms),
+    transitive_candidates(RelationTerms,PossiblyTransitivePairs),
+    all_transitive(RelationTerms,PossiblyTransitivePairs).
 
-possibly_transitive_pair(TermList,Term1,Term2) :-
-    member(Term1,TermList),
-    member(Term2,TermList),
-    Term1 =.. [FunctorName|[Arg1,Arg2]],
-    Term2 =.. [FunctorName|[Arg2,Arg3]],
+possibly_transitive_pair(RelationTerms,Term1,Term2) :-
+    member(Term1,RelationTerms),
+    member(Term2,RelationTerms),
+    Term1 =.. [RelationName|[Arg1,Arg2]],
+    Term2 =.. [RelationName|[Arg2,Arg3]],
     \+ (Arg1 = Arg2),
     \+ (Arg2 = Arg3),
     \+ (Arg1 = Arg3).
 
 transitive_terms(Term1,Term2,Term3) :-
-    Term1 =.. [FunctorName|[Arg1,Arg2]],
-    Term2 =.. [FunctorName|[Arg2,Arg3]],
-    Term3 =.. [FunctorName|[Arg1,Arg3]],
+    Term1 =.. [RelationName|[Arg1,Arg2]],
+    Term2 =.. [RelationName|[Arg2,Arg3]],
+    Term3 =.. [RelationName|[Arg1,Arg3]],
     \+ (Arg1 = Arg2),
     \+ (Arg2 = Arg3),
     \+ (Arg1 = Arg3).
 
-transitive_candidates(TermList,PossiblyTransitivePairs) :-
-    findall([Term1,Term2],possibly_transitive_pair(TermList,Term1,Term2),PossiblyTransitivePairs).
+transitive_candidates(RelationTerms,PossiblyTransitivePairs) :-
+    findall([Term1,Term2],possibly_transitive_pair(RelationTerms,Term1,Term2),PossiblyTransitivePairs).
 
 all_transitive(_,[]).
-all_transitive(TermList,[[Term1,Term2]|PossiblyTransitivePairs]) :-
-    member(Term3,TermList),
+all_transitive(RelationTerms,[[Term1,Term2]|PossiblyTransitivePairs]) :-
+    member(Term3,RelationTerms),
     transitive_terms(Term1,Term2,Term3),
-    all_transitive(TermList,PossiblyTransitivePairs).
+    all_transitive(RelationTerms,PossiblyTransitivePairs).
 
 %Strict partial order test
-strict_partial_order(FunctorName) :-
-    find_all_binary_relations(FunctorName,TermList),
-    irreflexive_terms(TermList,TermList),
-    transitive_candidates(TermList,PossiblyTransitivePairs),
-    all_transitive(TermList,PossiblyTransitivePairs).
+poset(RelationName) :-
+    find_all_binary_relations(RelationName,RelationTerms),
+    reflexive_terms(RelationTerms,RelationTerms),
+    antisymmetric_terms(RelationTerms,RelationTerms),
+    transitive_candidates(RelationTerms,PossiblyTransitivePairs),
+    all_transitive(RelationTerms,PossiblyTransitivePairs).
+
+%Lower and Upper Bounds
+lower_bound(RelationName,Subset,LowerBound) :-
+    poset(RelationName),
+    find_all_binary_relations(RelationName,PosetTerms),
+    gather_binary_args(PosetTerms,PosetArgs),
+    set(Subset),
+    set_inclusion(Subset,PosetArgs),
+    member(LowerBound,PosetArgs),
+    check_lower_bound(RelationName,PosetTerms,Subset,LowerBound).
+
+check_lower_bound(_,_,[],LowerBound).
+check_lower_bound(RelationName,PosetTerms,[Arg|Args],LowerBound) :-
+    LowerBoundTemplate =.. [RelationName|[LowerBound,Arg]],
+    member(LowerBoundTemplate,PosetTerms),
+    check_lower_bound(RelationName,PosetTerms,Args,LowerBound).
